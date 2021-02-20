a3ba2eed6a2969d4290264d0de1f3b1006f07a22
Erik.Wu <erik.wu@titansoft.com.sg>
2021-02-20 15:34:37 +0800

    add fake item

--- budgetCalculator/budgetCalculator/UnitTest1.cs
+++ budgetCalculator/budgetCalculator/UnitTest1.cs
@@ -12,7 +12,7 @@ namespace budgetCalculator
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+
         }
     }
 }
---
+++ budgetCalculator/budgetCalculator/budgetCalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using NSubstitute.Core;
+using NUnit.Framework;
+
+namespace budgetCalculator
+{
+    public class BudgetCalculatorTests
+    {
+        private BudgetCalculator _budgetCalculator;
+        private readonly IBudgetRepo _budgetRepo;
+
+        public BudgetCalculatorTests(IBudgetRepo budgetRepo)
+        {
+            _budgetRepo = Substitute.For<IBudgetRepo>();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            _budgetCalculator = new BudgetCalculator();
+        }
+        [Test]
+        public void First_test()
+        {
+            var budgetList = new List<Budget>
+            {
+                new Budget()
+                {
+                    Amount = 31,
+                    YearMonth = "202101"
+                }
+            };
+            GivenBudgetList(budgetList);
+        }
+
+        private void GivenBudgetList(List<Budget> returnThis)
+        {
+            _budgetRepo.GetAll().Returns(returnThis);
+        }
+    }
+
+    public class BudgetRepo : IBudgetRepo
+    {
+        public List<Budget> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+
+    public class Budget
+    {
+        public string YearMonth { get; set; }
+        public int Amount { get; set; }
+    }
+
+    public class BudgetCalculator
+    {
+    }
+}
