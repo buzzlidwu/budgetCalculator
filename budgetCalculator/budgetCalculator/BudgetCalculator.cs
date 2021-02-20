1028bdbf3840b7d53289b2116412c1783f690961
james732 <james732@gmail.com>
2021-02-20 16:39:57 +0800

    convert to datetime

--- budgetCalculator/budgetCalculator/BudgetCalculator.cs
+++ budgetCalculator/budgetCalculator/BudgetCalculator.cs
@@ -16,17 +16,24 @@ namespace budgetCalculator
 
         class BudgetData
         {
-            public DateTime dateTime { get; set; }
+            public DateTime DT { get; set; }
             public int Amount { get; set; }
         }
 
-        public decimal Query(DateTime start, DateTime end)
+        public int Query(DateTime start, DateTime end)
         {
-            List<string> strs = new List<string>();
-            strs.Add(start.ToString("YYYYMM"));
-            strs.Add(end.ToString("YYYYMM"));
+            var dataList = new List<BudgetData>();
+            foreach (var d in data_source.GetAll())
+            {
+                BudgetData bd = new BudgetData() {
+                    DT = DateTime.ParseExact(d.YearMonth, "yyyyMM", null),
+                    Amount = d.Amount };
+                dataList.Add(bd);
 
-            data_source.GetAll().Where(x => strs.Contains(x.YearMonth));
+                //DateTime.DaysInMonth
+            }
+            var diff = (end - start).TotalDays();
+
             return 0;
         }
     }
