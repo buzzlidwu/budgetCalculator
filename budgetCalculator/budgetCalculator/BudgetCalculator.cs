68cf81b9c620d8946f6a971ea9b329d484fd35d6
james732 <james732@gmail.com>
2021-02-20 15:49:38 +0800

    add query

--- budgetCalculator/budgetCalculator/BudgetCalculator.cs
+++ budgetCalculator/budgetCalculator/BudgetCalculator.cs
@@ -1,6 +1,21 @@
-﻿namespace budgetCalculator
+﻿using budgetCalculator.Interface;
+using System;
+
+namespace budgetCalculator
 {
     public class BudgetCalculator
     {
+        IBudgetRepo data_source;
+
+        public BudgetCalculator(IBudgetRepo repo)
+        {
+            data_source = repo;
+        }
+
+        public decimal Query(DateTime start, DateTime end)
+        {
+            var data = data_source.GetAll();
+            return 0;
+        }
     }
 }
