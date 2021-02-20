4410cabcba0534ef313d610126b69554494bde54
LAPTOP-0JG46SA9\Joanne.Chuang <riejoanne@yahoo.com.tw>
2021-02-20 17:31:22 +0800

    undone 判斷

--- budgetCalculator/budgetCalculator/BudgetCalculator.cs
+++ budgetCalculator/budgetCalculator/BudgetCalculator.cs
@@ -22,19 +22,93 @@ namespace budgetCalculator
 
         public int Query(DateTime start, DateTime end)
         {
-            var dataList = new List<BudgetData>();
-            foreach (var d in data_source.GetAll())
-            {
-                BudgetData bd = new BudgetData() {
-                    DT = DateTime.ParseExact(d.YearMonth, "yyyyMM", null),
-                    Amount = d.Amount };
-                dataList.Add(bd);
+            var monthDetail = new List<MonthDetail>();
+            var monthRange = getMonthRange(start, end);
 
-                //DateTime.DaysInMonth
+            monthDetail.Add(new MonthDetail()
+            {
+                Year = start.Year,
+                Month = start.Month,
+                TotalDays = MathStartDateMonthDays(start)
+            });
+
+            for (int i = 1; i < monthRange; i++)
+            {
+                var currentMonth = start.AddMonths(i);
+                monthDetail.Add(new MonthDetail()
+                {
+                    Year = currentMonth.Year,
+                    Month = currentMonth.Month,
+                    TotalDays = MathStartDateMonthDays(currentMonth)
+                });
             }
-            var diff = (end - start).TotalDays();
+
+            monthDetail.Add(new MonthDetail()
+            {
+                Year = end.Year,
+                Month = end.Month,
+                TotalDays = end.Day
+            });
+
+            var budgets = data_source.GetAll();
+            var total = 0;
+            foreach (var month in monthDetail)
+            {
+                var currentMonthsBudget = budgets.Find(budget =>
+                 {
+                     var time = Convert.ToDateTime(budget.YearMonth);
+                     return time.Year == month.Year && time.Month == month.Month;
+                 });
+
+                //if(
+            }
+
+
+            //int nStart = Convert.ToInt16(start.ToString("yyyyMM"));
+            //int nEnd = Convert.ToInt16(end.ToString("yyyyMM"));
+            //data_source.GetAll().Where()
+            //foreach (var d in data_source.GetAll())
+            //{
+            //    BudgetData bd = new BudgetData() {
+            //        DT = DateTime.ParseExact(d.YearMonth, "yyyyMM", null),
+            //        Amount = d.Amount };
+            //    dataList.Add(bd);
+
+            //    //DateTime.DaysInMonth
+            //}
+            //var diff = (end - start).TotalDays();
 
             return 0;
         }
+
+        private static int getMonthRange(DateTime start, DateTime end)
+        {
+            int monthRange;
+            if (start.Year == end.Year)
+            {
+                monthRange = end.Month - start.Month - 1;
+            }
+            else
+            {
+                var startMonth = 12 - start.Month;
+                monthRange = startMonth + end.Month - 1;
+            }
+
+            return monthRange;
+        }
+
+        private int MathStartDateMonthDays(DateTime start)
+        {
+            var dataList = new List<BudgetData>();
+            var startDateMonthDays = DateTime.DaysInMonth(start.Year, start.Month);
+            return startDateMonthDays - start.Day + 1;
+        }
+    }
+
+    public class MonthDetail
+    {
+        public int Year { get; set; }
+        public int Month { get; set; }
+        public int TotalDays { get; set; }
     }
 }
